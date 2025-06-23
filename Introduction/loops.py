7041916475b68c6b814dc7f86a073abf383739d3
Omi Mishra <work.omimishra@gmail.com>
2025-06-23 19:04:48 +0530

    #Loops in python

---
+++ Introduction/loops.py
@@ -0,0 +1,6 @@
+# The list of non-negative integers that are less than n . Print the square of each number on a separate line.
+# https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+if __name__ == '__main__':
+    n = int(input())
+for i in range (n):
+    print(i*i)
